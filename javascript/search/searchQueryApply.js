59b5d175478a436cff77adf1682fecce1cc55686
M.O.K.K.A <marbeloz.studio@gmail.com>
2023-10-27 12:48:54 +0200

    alternative search algo

--- javascript/search/searchQueryApply.js
+++ javascript/search/searchQueryApply.js
@@ -3,23 +3,32 @@ import { checkIfInfoIsSearched } from "../utils/common.js";
 
 //Exports
 export default function searchQueryApply(results, searchQuery) {
-    return searchQuery.length < 3 //The search query isn't applied if it contains less than 3 letters
-        ? results
-        : results.filter((recipe) => {
-              //Name, description and ingredients are counted in the main search bar
-              if (checkIfInfoIsSearched(recipe.name, searchQuery)) {
-                  return true;
-              }
-              if (checkIfInfoIsSearched(recipe.description, searchQuery)) {
-                  return true;
-              }
-              for (let ingredient of recipe.ingredients) {
-                  if (
-                      checkIfInfoIsSearched(ingredient.ingredient, searchQuery)
-                  ) {
-                      return true;
-                  }
-              }
-              return false;
-          });
+    if (searchQuery.length < 3) {
+        //The search query isn't applied if it contains less than 3 letters
+        return results;
+    }
+    const validRecipes = [];
+    for (let i = 0; i < results.length; i++) {
+        const recipe = results[i];
+        //Name, description and ingredients are counted in the main search bar
+        if (checkIfInfoIsSearched(recipe.name, searchQuery)) {
+            validRecipes.push(recipe);
+        } else {
+            if (checkIfInfoIsSearched(recipe.description, searchQuery)) {
+                validRecipes.push(recipe);
+            } else {
+                for (let ingredient of recipe.ingredients) {
+                    if (
+                        checkIfInfoIsSearched(
+                            ingredient.ingredient,
+                            searchQuery
+                        )
+                    ) {
+                        validRecipes.push(recipe);
+                    }
+                }
+            }
+        }
+    }
+    return validRecipes;
 }
