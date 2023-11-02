feb861667bb9a263a32df89d47d6da4647e3745d
M.O.K.K.A <marbeloz.studio@gmail.com>
2023-11-02 17:58:56 +0100

    search performance improvements

--- javascript/search/searchQueryApply.js
+++ javascript/search/searchQueryApply.js
@@ -25,6 +25,7 @@ export default function searchQueryApply(results, searchQuery) {
                         )
                     ) {
                         validRecipes.push(recipe);
+                        break;
                     }
                 }
             }
